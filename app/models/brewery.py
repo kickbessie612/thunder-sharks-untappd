8ad1c0890e7f9a2b911afd95ed8219066ddda968
Cory Baker <corybaker24@gmail.com>
2023-02-25 14:56:52 -0800

    brewery model to be added

--- app/models/brewery.py
+++ app/models/brewery.py
@@ -1,1 +1,38 @@
-HElllo Brewery
+
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+class Brewery(db.Model):
+    __tablename__ = 'breweries'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False, unique=True)
+    address = db.Column(db.String(255), nullable=False, unique=True)
+    city = db.Column(db.String(255), nullable=False)
+    state = db.Column(db.String(255), nullable=False)
+    country = db.Column(db.String(255), nullable=False)
+    type = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    picture = db.Column(db.String(255), nullable=False)
+
+    beers = db.relationship('Beer', back_populates='brewery', lazy=True)
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'address': self.address,
+            'city': self.city,
+            'state': self.state,
+            'country': self.country,
+            'type': self.type,
+            'description': self.description,
+            'picture': self.picture,
+            'beers': self.beers
+        }
