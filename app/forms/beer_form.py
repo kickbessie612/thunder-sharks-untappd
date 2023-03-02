784d69beeef23271b0ac2fc5292cf3d09f194a5c
Cory Baker <corybaker24@gmail.com>
2023-03-01 20:19:34 -0800

    BeerForm has been created

---
+++ app/forms/beer_form.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, IntegerField, TextAreaField, SubmitField
+from wtforms.validators import Length, DataRequired, NumberRange
+
+class BeerForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(max=50)])
+    description = TextAreaField('Description', validators=[DataRequired(), Length(max=255)])
+    abv = FloatField('ABV', validators=[DataRequired(), Length(max=255), NumberRange(min=0, max=100)])
+    ibu = FloatField('IBU', validators=[DataRequired(), NumberRange(min=0, max=1000), NumberRange(min=0, max=1000)])
+    style = StringField('Style', validators=[DataRequired(), Length(max=255)])
+    label = StringField('Label', validators=[DataRequired(), Length(max=255)])
+    year = IntegerField('Year', validators=[DataRequired()])
+    submit = SubmitField('Submit Beer')
