4cd0781b53e077a53bc4fd5282098ca5d975a94f
erenali22 <erena.li@outlook.com>
2023-03-01 20:58:04 -0800

    review form

---
+++ app/models/review.py
@@ -0,0 +1,27 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Float, nullable=False)
+    comment = db.Column(db.String(200), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    beer_id = db.Column(db.Integer, db.ForeignKey('beers.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship('User', back_populates='reviews')
+    beer = db.relationship('Beer', back_populates='reviews')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'rating': self.rating,
+            'comment': self.comment,
+            'createdAt': self.created_at
+        }
