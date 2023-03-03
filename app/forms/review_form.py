154369a6fe26d17fac67276058137b97e749393f
erenali22 <erena.li@outlook.com>
2023-03-02 19:40:32 -0800

    review form updated again

--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Length, NumberRange
 
 class AddReviewForm(FlaskForm):
     body = StringField('Body', validators=[DataRequired()])
-    images = StringField('Images', validators=[Length(max=1000)])
+    images = StringField('Images', validators=[Length(max=500)])
     ratings = FloatField('Ratings', validators=[DataRequired(), NumberRange(min=0, max=5)])
     user_id = IntegerField('User ID', validators=[DataRequired(), NumberRange(min=1)])
     beer_id = IntegerField('Beer ID', validators=[DataRequired(), NumberRange(min=1)])
