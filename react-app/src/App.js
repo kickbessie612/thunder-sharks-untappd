c7f1ebf7d570ba801c5562fdde812f396e35c4ac
CavinTimothy <cavintimotht2014@gmail.com>
2023-03-06 20:25:23 -0800

    Added demo user button

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
-import SignupFormPage from './components/SignupFormPage';
-import LoginFormPage from './components/LoginFormPage';
 import { authenticate } from './store/session';
 import Navigation from './components/Navigation';
 
@@ -27,14 +25,6 @@ function App() {
         <Switch>
           <Route exact path='/'>
             <BeerIndex />
-          </Route>
-
-          <Route path='/login'>
-            <LoginFormPage />
-          </Route>
-
-          <Route path='/signup'>
-            <SignupFormPage />
           </Route>
 
           <Route path='/beers/new'>
