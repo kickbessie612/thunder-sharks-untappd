1a2486cf478284fb8e89e38a5943859617e1e827
CavinTimothy <cavintimotht2014@gmail.com>
2023-03-06 19:43:45 -0800

    Added logo to navbar

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -4,20 +4,30 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
-function Navigation({ isLoaded }){
+function Navigation({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
 
+	// return (
+	// 	<ul>
+	// 		<li>
+	// 			<NavLink exact to="/">Home</NavLink>
+	// 		</li>
+	// 		{isLoaded && (
+	// 			<li>
+	// 				<ProfileButton user={sessionUser} />
+	// 			</li>
+	// 		)}
+	// 	</ul>
+	// );
+
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
+		<div>
+			<nav>
+				<NavLink exact to="/"><i class="fa-brands fa-untappd">UNTAPPD</i></NavLink>
+				{isLoaded && (<ProfileButton user={sessionUser} />)}
+			</nav>
+		</div>
+
 	);
 }
 
