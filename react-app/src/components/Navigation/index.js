1dc3d17f786e7422f6acdaf1274f82c1ed31fda7
CavinTimothy <cavintimotht2014@gmail.com>
2023-03-06 19:57:14 -0800

    Added logo to navbar

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -7,27 +7,11 @@ import './Navigation.css';
 function Navigation({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
 
-	// return (
-	// 	<ul>
-	// 		<li>
-	// 			<NavLink exact to="/">Home</NavLink>
-	// 		</li>
-	// 		{isLoaded && (
-	// 			<li>
-	// 				<ProfileButton user={sessionUser} />
-	// 			</li>
-	// 		)}
-	// 	</ul>
-	// );
-
 	return (
-		<div>
-			<nav>
-				<NavLink exact to="/"><i class="fa-brands fa-untappd">UNTAPPD</i></NavLink>
-				{isLoaded && (<ProfileButton user={sessionUser} />)}
-			</nav>
-		</div>
-
+		<nav>
+			<NavLink exact to="/"><i class="fa-brands fa-untappd">UNTAPPD</i></NavLink>
+			{isLoaded && (<ProfileButton user={sessionUser} />)}
+		</nav>
 	);
 }
 
