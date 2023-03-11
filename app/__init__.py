31ae53ed911b62119b68300d0581d924dd3333e0
Yuan Wang <yuanwang612@gmail.com>
2023-03-11 11:48:00 -0800

    ready to deploy

--- app/__init__.py
+++ app/__init__.py
@@ -80,6 +80,11 @@ def api_help():
     return route_list
 
 
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def react_root(path):
@@ -91,13 +96,3 @@ def react_root(path):
     if path == 'favicon.ico':
         return app.send_from_directory('public', 'favicon.ico')
     return app.send_static_file('index.html')
-
-
-@app.errorhandler(404)
-def not_found(e):
-    return app.send_static_file('index.html')
-
-
-@app.route('/home')
-def index():
-    return "Untapped clone"
