9fc91f8c84f9dd5460391aed39a11ebf893caa45
Yuan Wang <yuanwang612@gmail.com>
2023-03-13 11:10:38 -0700

    update migration env for deploying

--- migrations/env.py
+++ migrations/env.py
@@ -7,6 +7,13 @@ from flask import current_app
 
 from alembic import context
 
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get('SCHEMA')
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -85,7 +92,11 @@ def run_migrations_online():
                 directives[:] = []
                 logger.info('No changes in schema detected.')
 
-    connectable = get_engine()
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool,
+    )
 
     with connectable.connect() as connection:
         context.configure(
@@ -95,7 +106,14 @@ def run_migrations_online():
             **current_app.extensions['migrate'].configure_args
         )
 
+        # Create a schema (only in production)
+        if environment == "production":
+            connection.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA}")
+
+        # Set search path to your schema (only in production)
         with context.begin_transaction():
+            if environment == "production":
+                context.execute(f"SET search_path TO {SCHEMA}")
             context.run_migrations()
 
 
