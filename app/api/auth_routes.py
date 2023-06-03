8b1280cd62d78ecfe15503c0f5b09df130d051b5
Cory Baker <corybaker24@gmail.com>
2023-06-03 16:07:32 -0700

    fixed unauthorized

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -81,4 +81,4 @@ def unauthorized():
     """
     Returns unauthorized JSON when flask-login authentication fails
     """
-    return {'errors': ['Unauthorized']}, 401
+    return {'errors': ['Please Log in  or Sign up']}, 401
